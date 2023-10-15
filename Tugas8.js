bb5583edbb2baed3b5939529e31f2a38a4b19d7a
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-16 01:00:46 +0700

    array object ganti nama default
    array object ganti nama default

---
+++ Tugas8.js
@@ -0,0 +1,46 @@
+function teman() {
+    var Kost = {
+            Nama        : "Carolina",
+            Angkatan    : 2019,
+            Fakultas    : "Pertanian",
+            Jurusan     : "Teknik Informatika",
+            Konsentrasi : "Software Engineering",
+            Wali_Studi  : "Amusi Lie",
+        }
+    var Kost1 =
+        {
+            Nama        : "Nolis Sanjaya",
+            Angkatan    : 2019,
+            Fakultas    : "FTI",
+            Jurusan     : "Teknik Informatika",
+            Konsentrasi : "Software Enginerring",
+            Wali_Studi  : "Hendri Yong",
+        }
+    
+    var Kost2 = {
+            Nama        : "Imanuel Budiman",
+            Angkatan    : 2017,
+            Fakultas    : "FH",
+            Jurusan     : "ilmu Hukum",
+            Konsentrasi : "Perdata",
+            Wali_Studi  : "Nolis Hendriko",
+    }
+
+    var Kost3 = {
+            Nama        : "Samuel Kiran",
+            Angkatan    : 2019,
+            Fakultas    : "FTI",
+            Jurusan     : "Teknik Informatika",
+            Konsentrasi : "Software Enginerring",
+            Wali_Studi  : "Jefri Bing",
+    }
+    
+
+    Kost.Nama = "CZ" 
+    Kost.Angkatan = 2018
+    console.log(Kost);
+    // console.log(Kost1);
+    // console.log(Kost2);
+    // console.log(Kost3);
+}
+teman();
